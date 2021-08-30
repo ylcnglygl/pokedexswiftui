0346ed92a1d3e90a76dbde4a99cdba8e2576a6a7
yalcin.golayoglu <yalcin.golayoglu@niltekyazilim.com>
2021-08-30 21:14:16 +0300

    Initial Commit

---
+++ Shared/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  Shared
+//
+//  Created by Yalçın Golayoğlu on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
